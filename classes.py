036f97d2694257c6af1075cf9b9f74a585db4b93
Omar Kobrosli <okobrosli2@gmail.com>
2025-07-03 15:52:32 +0300

    Added student class.

--- classes.py
+++ classes.py
@@ -5,7 +5,7 @@ class User:
     
     def get_info(self):
         # Display Info
-        print(f"Name: {self.__name}, email: {self.__email}")
+        print(f"Name: {self.__name}, email: {self.__email} ")
         
 
     def set_email(self, email):
@@ -13,3 +13,16 @@ class User:
         self.__email = email
         
         
+class Student(User):
+    def __init__(self, name, email):
+        super().__init__(name, email)
+        self.__enrolled_courses = []
+        
+    def enroll(self,course_name):
+        self.__enrolled_courses.append(course_name)
+        
+    def get_student(self):
+        # Display Info
+        self.get_info()
+        print(f"Enrolled Courses : {self.__enrolled_courses}")
+
